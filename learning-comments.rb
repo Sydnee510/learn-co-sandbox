4aad5e3e8cb3ee20fdf1624e88addb2ca7f5b8ca
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-10-08 22:52:19 +0000

    set up my files here and added attributes to the player class

---
+++ learning-comments.rb
@@ -0,0 +1,4 @@
+# Your birthday is coming up "Shawn"
+name = "Lisa"
+
+puts "I am so happy that you invited me to your birthday, #{name} will be joining us."
---
+++ looping.rb
@@ -0,0 +1,7 @@
+def looping
+  loop do 
+    puts "Never pass a school bus if it has flashing red lights"
+  end
+end  
+looping
+#This is an infinite loop because we never included a counter nor a break statement
